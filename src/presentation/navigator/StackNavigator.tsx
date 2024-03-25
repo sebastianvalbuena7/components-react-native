94627ee3eee7d77c0344b50a03865c619765d661
Sebastian <sebastianvalbuena25@gmail.com>
2024-03-25 17:16:18 -0500

    Menu terminado

--- src/presentation/navigator/StackNavigator.tsx
+++ src/presentation/navigator/StackNavigator.tsx
@@ -1,11 +1,13 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import { HomeScreen } from '../screens/home/Home';
+import { HomeScreen } from '../screens/home/HomeScreen';
 
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+            headerShown: false
+        }}>
             <Stack.Screen name="Home" component={HomeScreen} />
             {/* <Stack.Screen name="Notifications" component={Notifications} />> */}
         </Stack.Navigator>
